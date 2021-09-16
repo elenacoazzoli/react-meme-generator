4553c143f2bf3787720ed19856c6478e2ed550cc
elenacoazzoli <coazzoli.elena@gmail.com>
2021-09-16 23:01:27 +0200

    Creates meme preview based on user inputs and downloads image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Button from './Button';
 
 function App() {
   const [upperText, setUpperText] = useState('');
   const [lowerText, setLowerText] = useState('');
   const [memeChoice, setMemeChoice] = useState('');
   const [allOptions, setAllOptions] = useState([]);
+  const [imageUrl, setImageUrl] = useState('');
 
   // Get all templates from the API and save all objects in allOptions array
   const getMemesTemplates = async () => {
@@ -26,9 +28,38 @@ function App() {
     getMemesTemplates();
   });
 
-  const previewMeme = () => {
-    console.log(memeChoice + ' ' + upperText + ' ' + lowerText);
+  const previewMeme = async () => {
+    setImageUrl(
+      'https://api.memegen.link/images/' +
+        memeChoice +
+        '/' +
+        upperText.replace(/ /g, '-') +
+        '/' +
+        lowerText.replace(/ /g, '-'),
+    );
+    await axios
+      .get(imageUrl)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) =>
+        // handle error
+        console.error(error),
+      );
   };
+
+  async function downloadImage() {
+    const image = await fetch(imageUrl);
+    const imageBlob = await image.blob();
+    const imagePath = URL.createObjectURL(imageBlob);
+
+    const link = document.createElement('a');
+    link.href = imagePath;
+    link.download = memeChoice + '_' + upperText + '_' + lowerText;
+    document.body.appendChild(link); // adds link at the end of the body
+    link.click(); // clicks link and downloads
+    document.body.removeChild(link); // removes link at the end of the body
+  }
 
   return (
     <div className="App">
@@ -67,13 +98,14 @@ function App() {
         <br />
         <br />
 
-        <button type="button" onClick={() => previewMeme()}>
-          Preview your meme
-        </button>
+        <Button onClick={() => previewMeme()} text="Preview your meme" />
+        <br />
+        <br />
 
-        <p>{memeChoice}</p>
-        <p>{upperText}</p>
-        <p>{lowerText}</p>
+        <img alt="" src={imageUrl} width="500px" />
+        <br />
+        <br />
+        <Button onClick={() => downloadImage()} text="Download your meme" />
       </div>
     </div>
   );
