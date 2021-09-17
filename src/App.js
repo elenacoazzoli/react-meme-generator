8dfacfcd2d015463accb22f7c4dd232ae3c1e9cf
elenacoazzoli <coazzoli.elena@gmail.com>
2021-09-17 12:39:32 +0200

    Adds Emotion CSS styles with Emotion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Button from './Button';
@@ -63,49 +65,103 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Meme Generator</h1>
-      <br />
-      <div>
-        <label htmlFor="memeList">Choose your favourite meme:</label>
-        <br />
-        <input
-          id="memeList"
-          list="memeOptions"
-          onChange={(event) => setMemeChoice(event.target.value)}
-        />
+      <h1
+        css={css({
+          color: '#9F84BD',
+        })}
+      >
+        Meme Generator
+      </h1>
+      <div
+        css={css({
+          padding: 10,
+          borderRadius: '3px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        })}
+      >
+        <div
+          css={css({
+            margin: 'auto',
+            padding: 20,
+            backgroundColor: '#EFD0CA',
+            borderRadius: '3px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          })}
+        >
+          <label htmlFor="memeList">Choose your favourite meme:</label>
 
-        <datalist id="memeOptions">
-          {allOptions.map((option) => (
-            <option key={option.id} value={option.id}>
-              {option.name}
-            </option>
-          ))}
-        </datalist>
-        <br />
-        <label htmlFor="upperText">Text:</label>
-        <br />
-        <input
-          id="upperText"
-          onChange={(event) => setUpperText(event.target.value)}
-        />
-        <br />
-        <label htmlFor="lowerText">Lower text:</label>
-        <br />
-        <input
-          id="lowerText"
-          onChange={(event) => setLowerText(event.target.value)}
-        />
-        <br />
-        <br />
+          <input
+            id="memeList"
+            list="memeOptions"
+            onChange={(event) => setMemeChoice(event.target.value)}
+            css={css({
+              padding: 10,
+              margin: '10px 0',
+              borderRadius: '8px',
+              border: '0', // remove default border
+              borderBottom: '2px solid #9F84BD',
+            })}
+          />
 
-        <Button onClick={() => previewMeme()} text="Preview your meme" />
-        <br />
-        <br />
+          <datalist id="memeOptions">
+            {allOptions.map((option) => (
+              <option key={option.id} value={option.id}>
+                {option.name}
+              </option>
+            ))}
+          </datalist>
+          <br />
+          <label htmlFor="upperText">Choose the upper text:</label>
 
-        <img alt="" src={imageUrl} width="500px" />
+          <input
+            id="upperText"
+            placeholder="Upper text"
+            onChange={(event) => setUpperText(event.target.value)}
+            css={css({
+              padding: 10,
+              margin: '10px 0',
+              borderRadius: '8px',
+              border: '0', // remove default border
+              borderBottom: '2px solid #9F84BD',
+            })}
+          />
+          <br />
+          <label htmlFor="lowerText">Choose the lower text:</label>
+          <input
+            id="lowerText"
+            placeholder="Lower text"
+            onChange={(event) => setLowerText(event.target.value)}
+            css={css({
+              padding: 10,
+              margin: '10px 0',
+              borderRadius: '8px',
+              border: '0', // remove default border
+              borderBottom: '2px solid #9F84BD',
+            })}
+          />
+          <br />
+
+          <Button onClick={() => previewMeme()} text="Preview your meme" />
+          <br />
+
+          <Button onClick={() => downloadImage()} text="Download your meme" />
+        </div>
         <br />
         <br />
-        <Button onClick={() => downloadImage()} text="Download your meme" />
+        <div
+          css={css`
+            background-color: #eee;
+          `}
+        >
+          <img alt="" src={imageUrl} width="500px" />
+        </div>
+        <br />
+        <br />
       </div>
     </div>
   );
--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
 const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+  return (
+    <button
+      onClick={props.onClick}
+      css={css({
+        padding: 10,
+        border: 'none',
+        backgroundColor: '#9F84BD',
+        color: '#fff',
+        fontWeight: 600,
+        borderRadius: '5px',
+      })}
+    >
+      {props.text}
+    </button>
+  );
 };
 
 export default Button;
